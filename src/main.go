99f0c7c95a9d9e76c6d78a9da4704f1002798922
jimao <1982775886@qq.com>
2020-11-20 23:27:30 +0800

    push pop

--- src/main.go
+++ src/main.go
@@ -3,9 +3,15 @@ package main
 import (
 	"fmt"
 	"strconv"
+
+	"./luavalue"
 )
 
 func main() {
 	var num float64 = 3.14167
 	fmt.Println(strconv.FormatFloat(num, 'f', -1, 64))
+	i := luavalue.NewLuaInteger(0)
+	var itf luavalue.ILuaValue
+	itf = &i
+	fmt.Println(itf.GetBoolean())
 }
--- src/state/stack.go
+++ src/state/stack.go
@@ -6,22 +6,38 @@ import (
 
 // LuaStack Lua栈
 type LuaStack struct {
-	top   int64
+	top   int
 	slots []luavalue.ILuaValue
 }
 
-func (me *LuaStack) Push(value luavalue.ILuaValue) {
-	me.slots[me.top] = value
+// Size 获取栈容量
+func (me *LuaStack) Size() int {
+	return len(me.slots)
 }
 
+// Push 向栈顶压入一个Lua值
+func (me *LuaStack) Push(value luavalue.ILuaValue) {
+	if me.top >= me.Size() {
+		panic("LuaStack Push: 栈空间不足")
+	}
+	me.slots[me.top] = value
+	me.top++
+}
+
+// Pop 从栈顶弹出一个Lua值
 func (me *LuaStack) Pop() luavalue.ILuaValue {
+	if me.top < 1 {
+		panic("LuaStack Pop: 栈已为空")
+	}
 	me.top--
 	result := me.slots[me.top]
+	luaNil := luavalue.NewLuaNil()
+	me.slots[me.top] = &luaNil
 	return result
 }
 
 // AbsIndex 相对索引转换成为绝对索引（内部索引）
-func (me *LuaStack) AbsIndex(index int64) int64 {
+func (me *LuaStack) AbsIndex(index int) int {
 	if index >= 0 {
 		return index
 	}
@@ -29,21 +45,22 @@ func (me *LuaStack) AbsIndex(index int64) int64 {
 }
 
 // IndexIsValid 判断索引是否在有效范围之内（外部索引）
-func (me *LuaStack) IndexIsValid(index int64) bool {
+func (me *LuaStack) IndexIsValid(index int) bool {
 	absIndex := me.AbsIndex(index)
 	return absIndex > 0 && absIndex <= me.top
 }
 
 // Get 根据索引从栈内获取Lua值（外部索引）
-func (me *LuaStack) Get(index int64) luavalue.ILuaValue {
+func (me *LuaStack) Get(index int) luavalue.ILuaValue {
 	if me.IndexIsValid(index) {
 		return me.slots[me.AbsIndex(index)-1]
 	}
-	return me.slots[me.AbsIndex(index)-1]
+	result := luavalue.NewLuaNil()
+	return &result
 }
 
 // Set 设置Lua值到栈中指定索引（外部索引）
-func (me *LuaStack) Set(index int64, value luavalue.ILuaValue) {
+func (me *LuaStack) Set(index int, value luavalue.ILuaValue) {
 	if me.IndexIsValid(index) {
 		me.slots[me.AbsIndex(index)-1] = value
 		return
