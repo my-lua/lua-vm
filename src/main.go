8571e958c9b79655e09360e2af9ce87c6dee9856
jimao <1982775886@qq.com>
2020-11-21 00:08:31 +0800

    修改测试代码

--- src/main.go
+++ src/main.go
@@ -1,17 +1,8 @@
 package main
 
-import (
-	"fmt"
-	"strconv"
-
-	"./luavalue"
-)
+import "fmt"
 
 func main() {
-	var num float64 = 3.14167
-	fmt.Println(strconv.FormatFloat(num, 'f', -1, 64))
-	i := luavalue.NewLuaInteger(0)
-	var itf luavalue.ILuaValue
-	itf = &i
-	fmt.Println(itf.GetBoolean())
+	num := 3.99999999999
+	fmt.Println(int(num))
 }
