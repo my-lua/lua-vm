faa5ed979a3a83e50ad2d2f4ce800d51c9269a77
jimao <1982775886@qq.com>
2020-11-22 12:23:35 +0800

    lua state

--- src/state/state.go
+++ src/state/state.go
@@ -1,4 +1,33 @@
 package state
 
 // LuaState Lua状态实例
-type LuaState struct{}
+type LuaState struct {
+	stack *LuaStack
+}
+
+// PushValue s
+func (me *LuaState) PushValue(index int) {
+
+}
+
+// ReplaceValue s
+func (me *LuaState) ReplaceValue(index int) {
+
+}
+
+// Insert s
+func (me *LuaState) Insert(index int) {
+
+}
+
+// Remove s
+func (me *LuaState) Remove(index int) {
+
+}
+
+// NewLuaState 构造函数创建一个LuaState
+func NewLuaState() *LuaState {
+	return &LuaState{
+		stack: NewLuaStack(20),
+	}
+}
