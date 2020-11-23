cadee6428f74c12b8cfc626c0c600fc4f823ad93
jimao <1982775886@qq.com>
2020-11-23 18:27:03 +0800

    访问闭包的方法待实现

--- src/state/stateCall.go
+++ src/state/stateCall.go
@@ -13,5 +13,14 @@ func (me *LuaState) Load(chunk []byte, chunkName, mode string) int {
 
 // Call s
 func (me *LuaState) Call(nArgs, nResults int) {
+	closure := me.stack.Get(-nArgs - 1)
+	if closure.Type() == luavalue.LuaTypeFunction {
+		me.callLuaClosure(closure.(*luavalue.LuaClosure), nArgs, nResults)
+	}
+	panic("LuaState Call: 目标索引处的值并非函数")
+}
+
+// callLuaClosure s
+func (me *LuaState) callLuaClosure(closure *luavalue.LuaClosure, nArgs, nResults int) {
 
 }
