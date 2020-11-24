35d072fae2022e5c360e9ce911d04b8dacd09d1d
jimao <1982775886@qq.com>
2020-11-25 00:38:49 +0800

    完善函数运行

--- src/state/stateCall.go
+++ src/state/stateCall.go
@@ -27,6 +27,12 @@ func (me *LuaState) Call(nArgs, nResults int) {
 	panic("LuaState Call: 目标索引处的值并非函数")
 }
 
+func (me *LuaState) runLuaClosure() {
+	for {
+		break
+	}
+}
+
 // callLuaClosure 调用闭包
 func (me *LuaState) callLuaClosure(closure *luavalue.LuaClosure, nArgs, nResults int) {
 	nRegs := int(closure.Prototype().MaxStackSize())
@@ -37,18 +43,28 @@ func (me *LuaState) callLuaClosure(closure *luavalue.LuaClosure, nArgs, nResults
 	newStack.closure = closure
 
 	funcAndArgs := me.stack.PopN(nArgs + 1)
+	// 函数参数入栈
 	newStack.PushN(funcAndArgs[1:], nParams)
-	newStack.top = nArgs
+	// 根据函数原型内的MaxStackSize设置栈顶
+	newStack.top = nRegs
+	// 如果实际传入参数数量大于函数原型内定义的参数数量，且函数原型内isVararg为真
+	// 则收集varArgs参数到新的栈
 	if nArgs > nParams && isVararg {
 		newStack.varArgs = funcAndArgs[nParams+1:]
 	}
 
 	me.PushStack(newStack)
 	// 这里需要运行闭包
+	me.runLuaClosure()
 	me.PopStack()
 
 	if nResults > 0 {
-
+		// 获取函数运行后留在栈顶的参数
+		results := newStack.PopN(newStack.Top() - nRegs)
+		// 检查或尝试扩容主调栈
+		me.stack.Check(len(results))
+		// 推入主调栈，多退少补
+		me.stack.PushN(results, nResults)
 	}
 
 	fmt.Println(nRegs, nParams, isVararg)
---
+++ test/1.lua
@@ -0,0 +1,4 @@
+
+local a = 1
+-- local b = 2
+print("你好，世界", a)
